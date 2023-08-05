34c1a4d61457a7e78fc84a09cbc1bf46f1c0a2d2
hoangtien2k3qx1 <hoangtien2k3qx1@gmail.com>
2023-08-06 00:58:48 +0700

    update

---
+++ DOBCalc/app/src/main/java/com/tutorials/dobcalc/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.tutorials.dobcalc
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // 0123456789ABCDEF
+
+
+    }
+}
